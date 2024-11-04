ad006f243f517808ee7392b9d962edc9eed28ae3
Nikolay Petrov <sungon@gmail.com>
2024-11-04 17:16:29 -0500

    simple server comms

--- cmd/connet-server/main.go
+++ cmd/connet-server/main.go
@@ -1,7 +1,150 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"math/big"
+	"net"
+	"os"
+	"time"
+
+	"github.com/klev-dev/kleverr"
+	"github.com/quic-go/quic-go"
+)
 
 func main() {
-	fmt.Println("Welcome to connet-server")
+	if err := run(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "error while running: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	// pk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	// san := pkix.Extension{}
+	// san.Id = objectIdentifier
+	// san.Critical = false
+	// san.Value = []byte(fmt.Sprintf("CN=%s", cfg.Name))
+
+	template := &x509.Certificate{
+		SerialNumber: big.NewInt(time.Now().UnixMilli()),
+		Subject: pkix.Name{
+			Country:      []string{"US"},
+			Organization: []string{"Connet"},
+		},
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(90 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IsCA:         true,
+	}
+	certData, err := x509.CreateCertificate(rand.Reader, template, template, pub, priv)
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+
+	certPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certData,
+	})
+
+	b, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+	certKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: b,
+	})
+
+	cert, err := tls.X509KeyPair(certPEM, certKeyPEM)
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+
+	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:8443")
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+
+	tr := &quic.Transport{
+		Conn: conn,
+		// TODO review other options
+	}
+	l, err := tr.Listen(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		NextProtos:   []string{"quic-connet"},
+	}, &quic.Config{
+		KeepAlivePeriod: 25 * time.Second,
+	})
+	if err != nil {
+		return kleverr.Ret(err)
+	}
+
+	fmt.Println("Started connet-server")
+	defer fmt.Println("Stopped connet-server")
+	for {
+		conn, err := l.Accept(ctx)
+		if err != nil {
+			if errors.Is(err, quic.ErrServerClosed) {
+				break
+			}
+		}
+		stream, err := conn.AcceptStream(ctx)
+		if err != nil {
+			serr := fmt.Sprintf("could not accept: %v", err)
+			fmt.Fprintf(os.Stderr, "%s\n", serr)
+			conn.CloseWithError(200, serr)
+			continue
+		}
+
+		buff := make([]byte, 4)
+		if n, err := stream.Read(buff); err != nil {
+			serr := fmt.Sprintf("could not read: %v", err)
+			fmt.Fprintf(os.Stderr, "%s\n", serr)
+			conn.CloseWithError(201, serr)
+			continue
+		} else if n < len(buff) {
+			// TODO short read
+			fmt.Fprintf(os.Stderr, "short read\n")
+			conn.CloseWithError(202, "short read")
+			continue
+		}
+
+		if n, err := stream.Write(buff); err != nil {
+			serr := fmt.Sprintf("could not write: %v", err)
+			fmt.Fprintf(os.Stderr, "%s\n", serr)
+			conn.CloseWithError(203, serr)
+			continue
+		} else if n < len(buff) {
+			// TODO short write
+			fmt.Fprintf(os.Stderr, "short write\n")
+			conn.CloseWithError(204, "short write")
+			continue
+		}
+
+		time.Sleep(time.Second)
+		conn.CloseWithError(100, "closing conn")
+	}
+
+	return nil
 }
--- cmd/connet/main.go
+++ cmd/connet/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"crypto/tls"
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/mr-tron/base58"
+	"github.com/quic-go/quic-go"
+)
 
 func main() {
-	fmt.Println("Welcome to connet")
+	if err := run(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "error while running: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	conn, err := quic.DialAddr(ctx, "127.0.0.1:8443", &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-connet"},
+	}, &quic.Config{})
+	if err != nil {
+		return err
+	}
+	defer conn.CloseWithError(100, "all good")
+
+	stream, err := conn.OpenStream()
+	if err != nil {
+		return err
+	}
+	defer stream.Close()
+
+	outBuff := binary.BigEndian.AppendUint32(nil, 32)
+	if n, err := stream.Write(outBuff); err != nil {
+		serr := fmt.Sprintf("could not write: %v", err)
+		fmt.Fprintf(os.Stderr, "%s\n", serr)
+		conn.CloseWithError(203, serr)
+		return err
+	} else if n < len(outBuff) {
+		// TODO short write
+		fmt.Fprintf(os.Stderr, "short write\n")
+		conn.CloseWithError(204, "short write")
+		return fmt.Errorf("short write: %d", n)
+	}
+
+	buff := make([]byte, 4)
+	if n, err := stream.Read(buff); err != nil {
+		serr := fmt.Sprintf("could not read: %v", err)
+		fmt.Fprintf(os.Stderr, "%s\n", serr)
+		conn.CloseWithError(201, serr)
+		return err
+	} else if n < len(buff) {
+		// TODO short read
+		fmt.Fprintf(os.Stderr, "short read\n")
+		conn.CloseWithError(202, "short read")
+		return fmt.Errorf("short read: %d", n)
+	}
+
+	fmt.Printf("Sent: %s, Recv: %s", base58.Encode(outBuff), base58.Encode(buff))
+	return nil
 }
