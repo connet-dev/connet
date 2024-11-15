2459c4f0a9f7040fbb851efc4471a1ea8247a443
Nikolay Petrov <sungon@gmail.com>
2024-11-15 08:53:51 -0500

    reorganize tests

--- certc/self_test.go
+++ certc/self_test.go
@@ -3,6 +3,7 @@ package certc
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"io"
 	"net"
 	"testing"
@@ -38,76 +39,8 @@ func TestChain(t *testing.T) {
 	clientCert, err := client.TLSCert()
 	require.NoError(t, err)
 
-	udpConn, err := net.ListenUDP("udp4", &net.UDPAddr{Port: 12345})
-	require.NoError(t, err)
-	defer udpConn.Close()
-
-	l, err := quic.Listen(udpConn, &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		ClientCAs:    caPool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		NextProtos:   []string{"test"},
-	}, &quic.Config{})
-	require.NoError(t, err)
-	defer l.Close()
-
-	g, ctx := errgroup.WithContext(context.Background())
-	g.Go(func() error {
-
-		c, err := l.Accept(ctx)
-		if err != nil {
-			return kleverr.Ret(err)
-		}
-		s, err := c.AcceptStream(ctx)
-		if err != nil {
-			return kleverr.Ret(err)
-		}
-		defer s.Close()
-
-		buf := make([]byte, 1)
-		if _, err := io.ReadFull(s, buf); err != nil {
-			return kleverr.Ret(err)
-		}
-		if _, err := s.Write(buf); err != nil {
-			return kleverr.Ret(err)
-		}
-		return nil
-	})
-
-	c, err := quic.DialAddr(ctx, "127.0.0.1:12345", &tls.Config{
-		Certificates: []tls.Certificate{clientCert},
-		RootCAs:      caPool,
-		ServerName:   clientCert.Leaf.DNSNames[0],
-		NextProtos:   []string{"test"},
-	}, &quic.Config{})
-	require.NoError(t, err)
-	defer c.CloseWithError(0, "done")
-
-	g.Go(func() error {
-		s, err := c.OpenStreamSync(context.Background())
-		if err != nil {
-			return kleverr.Ret(err)
-		}
-		defer s.Close()
-
-		buf := make([]byte, 1)
-		buf[0] = 33
-		if _, err := s.Write(buf); err != nil {
-			return kleverr.Ret(err)
-		}
-		buf[0] = 0
-		if _, err := io.ReadFull(s, buf); err != nil {
-			return kleverr.Ret(err)
-		}
-		return nil
-	})
-
-	if err := g.Wait(); err != nil {
-		if kerr := kleverr.Get(err); kerr != nil {
-			t.Fatal(kerr.Print())
-		}
-		t.Fatal(err)
-	}
+	testConnectivity(t, serverCert, caPool, clientCert, caPool)
+	testConnectivityDyn(t, serverCert, caPool, clientCert, caPool)
 }
 
 func TestChainRoot(t *testing.T) {
@@ -138,16 +71,85 @@ func TestChainRoot(t *testing.T) {
 	clientCert, err := client.TLSCert()
 	require.NoError(t, err)
 
+	testConnectivity(t, serverCert, caPool, clientCert, caPool)
+	testConnectivityDyn(t, serverCert, caPool, clientCert, caPool)
+}
+
+func TestExchange(t *testing.T) {
+	serverRoot, err := NewRoot()
+	require.NoError(t, err)
+	serverCA, err := serverRoot.CertPool()
+	require.NoError(t, err)
+
+	serverCert, err := serverRoot.NewServer(CertOpts{
+		Domains: []string{"zzz"},
+	})
+	require.NoError(t, err)
+	serverTLS, err := serverCert.TLSCert()
+	require.NoError(t, err)
+
+	clientRoot, err := NewRoot()
+	require.NoError(t, err)
+	clientCA, err := clientRoot.CertPool()
+	require.NoError(t, err)
+
+	clientCert, err := clientRoot.NewClient(CertOpts{
+		Domains: []string{"zzz"},
+	})
+	require.NoError(t, err)
+	clientTLS, err := clientCert.TLSCert()
+	require.NoError(t, err)
+
+	testConnectivity(t, serverTLS, clientCA, clientTLS, serverCA)
+	testConnectivityDyn(t, serverTLS, clientCA, clientTLS, serverCA)
+}
+
+func testConnectivity(t *testing.T, serverCert tls.Certificate, clientCA *x509.CertPool, clientCert tls.Certificate, rootCA *x509.CertPool) {
+	serverConf := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientCAs:    clientCA,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		NextProtos:   []string{"test"},
+	}
+
+	clientConf := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      rootCA,
+		ServerName:   clientCert.Leaf.DNSNames[0],
+		NextProtos:   []string{"test"},
+	}
+
+	testConnectivityTLS(t, serverConf, clientConf)
+}
+
+func testConnectivityDyn(t *testing.T, serverCert tls.Certificate, clientCA *x509.CertPool, clientCert tls.Certificate, rootCA *x509.CertPool) {
+	serverConf := &tls.Config{
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		NextProtos: []string{"test"},
+	}
+	serverConf.GetConfigForClient = func(chi *tls.ClientHelloInfo) (*tls.Config, error) {
+		conf := serverConf.Clone()
+		conf.Certificates = []tls.Certificate{serverCert}
+		conf.ClientCAs = clientCA
+		return conf, nil
+	}
+
+	clientConf := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      rootCA,
+		ServerName:   clientCert.Leaf.DNSNames[0],
+		NextProtos:   []string{"test"},
+	}
+
+	testConnectivityTLS(t, serverConf, clientConf)
+}
+
+func testConnectivityTLS(t *testing.T, serverConf *tls.Config, clientConf *tls.Config) {
 	udpConn, err := net.ListenUDP("udp4", &net.UDPAddr{Port: 12345})
 	require.NoError(t, err)
 	defer udpConn.Close()
 
-	l, err := quic.Listen(udpConn, &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		ClientCAs:    caPool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		NextProtos:   []string{"test"},
-	}, &quic.Config{})
+	l, err := quic.Listen(udpConn, serverConf, &quic.Config{})
 	require.NoError(t, err)
 	defer l.Close()
 
@@ -173,12 +175,7 @@ func TestChainRoot(t *testing.T) {
 		return nil
 	})
 
-	c, err := quic.DialAddr(ctx, "127.0.0.1:12345", &tls.Config{
-		Certificates: []tls.Certificate{clientCert},
-		RootCAs:      caPool,
-		ServerName:   clientCert.Leaf.DNSNames[0],
-		NextProtos:   []string{"test"},
-	}, &quic.Config{})
+	c, err := quic.DialAddr(ctx, "127.0.0.1:12345", clientConf, &quic.Config{})
 	require.NoError(t, err)
 	defer c.CloseWithError(0, "done")
 
@@ -201,10 +198,6 @@ func TestChainRoot(t *testing.T) {
 		return nil
 	})
 
-	if err := g.Wait(); err != nil {
-		if kerr := kleverr.Get(err); kerr != nil {
-			t.Fatal(kerr.Print())
-		}
-		t.Fatal(err)
-	}
+	err = g.Wait()
+	require.NoError(t, err)
 }
