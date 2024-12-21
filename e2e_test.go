9ac75a99a4c10414e249571cfdaecef68f7a9219
Nikolay Petrov <sungon@gmail.com>
2024-12-20 20:28:41 -0500

    cover more e2e scenarious

--- e2e_test.go
+++ e2e_test.go
@@ -44,6 +44,12 @@ func TestE2E(t *testing.T) {
 		ClientDirectAddress(":19192"),
 		ClientDestination("direct", hts.Listener.Addr().String(), model.RouteDirect),
 		ClientDestination("relay", hts.Listener.Addr().String(), model.RouteRelay),
+		ClientDestination("dst-any-direct-src", hts.Listener.Addr().String(), model.RouteAny),
+		ClientDestination("dst-any-relay-src", hts.Listener.Addr().String(), model.RouteAny),
+		ClientDestination("dst-direct-any-src", hts.Listener.Addr().String(), model.RouteDirect),
+		ClientDestination("dst-relay-any-src", hts.Listener.Addr().String(), model.RouteRelay),
+		ClientDestination("dst-direct-relay-src", hts.Listener.Addr().String(), model.RouteDirect),
+		ClientDestination("dst-relay-direct-src", hts.Listener.Addr().String(), model.RouteRelay),
 		ClientLogger(logger.With("test", "cl-dst")),
 	)
 	require.NoError(t, err)
@@ -55,6 +61,12 @@ func TestE2E(t *testing.T) {
 		ClientDirectAddress(":19193"),
 		ClientSource("direct", ":9990", model.RouteDirect),
 		ClientSource("relay", ":9991", model.RouteRelay),
+		ClientSource("dst-any-direct-src", ":9992", model.RouteDirect),
+		ClientSource("dst-any-relay-src", ":9993", model.RouteRelay),
+		ClientSource("dst-direct-any-src", ":9994", model.RouteAny),
+		ClientSource("dst-relay-any-src", ":9995", model.RouteAny),
+		ClientSource("dst-direct-relay-src", ":9996", model.RouteRelay),
+		ClientSource("dst-relay-direct-src", ":9997", model.RouteDirect),
 		ClientLogger(logger.With("test", "cl-src")),
 	)
 	require.NoError(t, err)
@@ -68,15 +80,16 @@ func TestE2E(t *testing.T) {
 
 	g.Go(func() error { return clDst.Run(ctx) })
 	g.Go(func() error { return clSrc.Run(ctx) })
-	time.Sleep(100 * time.Millisecond) // time for clients to come online
+	time.Sleep(300 * time.Millisecond) // time for clients to come online
 
 	// actual test
 	httpcl := &http.Client{}
 	httpcl.Transport = &http.Transport{DisableKeepAlives: true}
 
-	ports := slices.Repeat([]int{9990, 9991}, 10)
+	// Positive
+	ports := slices.Repeat([]int{9990, 9991, 9992, 9993, 9994, 9995}, 3)
 	for i, port := range ports {
-		t.Run(fmt.Sprintf("%d:%d", i, port), func(t *testing.T) {
+		t.Run(fmt.Sprintf("success-%d:%d", i, port), func(t *testing.T) {
 			rnd := rand.Uint64()
 			url := fmt.Sprintf("http://localhost:%d?rand=%d", port, rnd)
 
@@ -88,6 +101,16 @@ func TestE2E(t *testing.T) {
 			require.NoError(t, err)
 
 			require.Equal(t, fmt.Sprintf("hello:%d", rnd), string(respData))
+		})
+	}
+
+	for i, port := range []int{9996, 9997} {
+		t.Run(fmt.Sprintf("failing-%d:%d", i, port), func(t *testing.T) {
+			rnd := rand.Uint64()
+			url := fmt.Sprintf("http://localhost:%d?rand=%d", port, rnd)
+
+			_, err := httpcl.Get(url)
+			require.Error(t, err)
 		})
 	}
 
