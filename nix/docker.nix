1d7571cd4516979934ec5449333f1b6bbd60df02
Nikolay Petrov <sungon@gmail.com>
2025-01-08 21:00:17 -0500

    update connet docker config

--- nix/docker.nix
+++ nix/docker.nix
@@ -6,5 +6,10 @@ pkgs.dockerTools.buildLayeredImage {
   name = "ghcr.io/connet-dev/connet";
   tag = "latest";
   contents = with pkgs; [ cacert ];
-  config.entrypoint = "${connet}/bin/connet";
+  config = {
+    Entrypoint = [ "${connet}/bin/connet" ];
+    Cmd = [ "--help" ];
+    Expose = [ "19190/udp" "19191/udp" "19192/udp" ];
+    Volume = "/data";
+  };
 }
