d090da41d0ce88ccd75b44920933315e1833c411
Nikolay Petrov <sungon@gmail.com>
2025-07-03 06:22:17 -0400

    simple loop

--- client/portmap.go
+++ client/portmap.go
@@ -3,13 +3,17 @@ package client
 import (
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net"
 	"net/netip"
+	"time"
 
+	"github.com/connet-dev/connet/notify"
 	"github.com/jackpal/gateway"
 	"github.com/quic-go/quic-go"
+	"golang.org/x/sync/errgroup"
 )
 
 const pmpBroadcastPort = 5350
@@ -23,6 +27,10 @@ type Portmapper struct {
 	gatewayAddr *net.UDPAddr
 	localIP     net.IP
 	localPort   uint16
+
+	externalAddr     *notify.V[*netip.Addr]
+	externalPort     *notify.V[*uint16]
+	externalAddrPort *notify.V[*netip.AddrPort]
 }
 
 func NewPortmapper(transport *quic.Transport, logger *slog.Logger) (*Portmapper, error) {
@@ -48,11 +56,124 @@ func NewPortmapper(transport *quic.Transport, logger *slog.Logger) (*Portmapper,
 		gatewayAddr: &net.UDPAddr{IP: gwIP, Port: pmpCommandPort},
 		localIP:     myIP,
 		localPort:   uint16(addr.Port),
+
+		externalAddr:     notify.NewEmpty[*netip.Addr](),
+		externalPort:     notify.NewEmpty[*uint16](),
+		externalAddrPort: notify.NewEmpty[*netip.AddrPort](),
 	}, nil
 }
 
 func (s *Portmapper) Run(ctx context.Context) error {
-	return nil
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return s.notifyExternalAddrPort(ctx)
+	})
+	g.Go(func() error {
+		return s.runDiscovery(ctx)
+	})
+	g.Go(func() error {
+		return s.runMap(ctx)
+	})
+	return g.Wait()
+}
+
+func (s *Portmapper) notifyExternalAddrPort(ctx context.Context) error {
+	addrCh := s.externalAddr.Notify(ctx)
+	portCh := s.externalPort.Notify(ctx)
+
+	var currentIP *netip.Addr
+	var currentPort *uint16
+	var update = func() {
+		if currentIP != nil && currentPort != nil {
+			newAddr := netip.AddrPortFrom(*currentIP, *currentPort)
+			s.externalAddrPort.Set(&newAddr)
+		} else {
+			s.externalAddrPort.Set(nil)
+		}
+	}
+	for {
+		select {
+		case currentIP = <-addrCh:
+			update()
+		case currentPort = <-portCh:
+			update()
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (s *Portmapper) runDiscovery(ctx context.Context) error {
+	defer s.externalAddr.Set(nil)
+
+	resp, err := retryCall(ctx, s.pmpDiscover)
+	if err != nil {
+		return err
+	}
+	s.externalAddr.Set(&resp.externalAddr)
+
+	return s.listenAddressChanges(ctx, resp.epochSeconds)
+}
+
+func (s *Portmapper) runMap(ctx context.Context) error {
+	defer s.externalPort.Set(nil)
+
+	resp, err := retryCall(ctx, func(ctx context.Context) (*pmpMapResponse, error) {
+		return s.pmpMap(ctx, 0, 60) // TODO change lifetime
+	})
+	if err != nil {
+		return err
+	}
+	s.externalPort.Set(&resp.externalPort)
+
+	for {
+		select {
+		case <-time.After(time.Duration(resp.lifetimeSeconds) * time.Second / 2):
+			resp, err = retryCall(ctx, func(ctx context.Context) (*pmpMapResponse, error) {
+				return s.pmpMap(ctx, resp.externalPort, 60)
+			})
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			_, merr := s.pmpMap(context.Background(), 0, 0)
+			return errors.Join(err, merr)
+		}
+	}
+}
+
+var errEpochReset = errors.New("router epoch reset")
+
+func (s *Portmapper) listenAddressChanges(ctx context.Context, epoch uint32) error {
+	var lc net.ListenConfig
+	conn, err := lc.ListenPacket(ctx, "udp4", "224.0.0.1:5350")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	var readResponse = func(ctx context.Context, buff []byte) (int, net.Addr, error) {
+		return conn.ReadFrom(buff)
+	}
+
+	for {
+		resp, err := s.readResponse(ctx, 12, readResponse)
+		if err != nil {
+			return fmt.Errorf("could not read packet: %w", err)
+		}
+		if err := checkResponseHeader(resp, 0); err != nil {
+			return fmt.Errorf("discovery check response: %w:", err)
+		}
+
+		nextEpoch := binary.BigEndian.Uint32(resp[4:])
+		externalAddr := netip.AddrFrom4([4]byte(resp[8:12]))
+
+		s.externalAddr.Set(&externalAddr)
+		if nextEpoch < epoch {
+			return errEpochReset
+		}
+		epoch = nextEpoch
+	}
 }
 
 type pmpDiscoverResponse struct {
@@ -69,7 +190,7 @@ func (s *Portmapper) pmpDiscover(ctx context.Context) (*pmpDiscoverResponse, err
 		return nil, fmt.Errorf("discovery write request: %w", err)
 	}
 
-	resp, err := s.readResponse(ctx, 12)
+	resp, err := s.readResponse(ctx, 12, s.transport.ReadNonQUICPacket)
 	if err != nil {
 		return nil, fmt.Errorf("discovery read response: %w", err)
 	}
@@ -101,7 +222,7 @@ func (s *Portmapper) pmpMap(ctx context.Context, desiredExternalPort uint16, map
 		return nil, fmt.Errorf("map write request: %w", err)
 	}
 
-	resp, err := s.readResponse(ctx, 16)
+	resp, err := s.readResponse(ctx, 16, s.transport.ReadNonQUICPacket)
 	if err != nil {
 		return nil, fmt.Errorf("map read response: %w", err)
 	}
@@ -130,9 +251,29 @@ func (s *Portmapper) writeRequest(request []byte) error {
 	return nil
 }
 
-func (s *Portmapper) readResponse(ctx context.Context, expectedSize int) ([]byte, error) {
+func retryCall[T any](ctx context.Context, fn func(ctx context.Context) (T, error)) (T, error) {
+	var errs []error
+	for i := range 9 {
+		timeout := time.Duration(250*i) * time.Millisecond
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		resp, err := fn(ctx)
+		if err == nil {
+			return resp, nil
+		}
+		errs = append(errs, err)
+	}
+
+	var t T
+	return t, errors.Join(errs...)
+}
+
+type readerFn = func(context.Context, []byte) (int, net.Addr, error)
+
+func (s *Portmapper) readResponse(ctx context.Context, expectedSize int, rdr readerFn) ([]byte, error) {
 	resp := make([]byte, 16)
-	m, respAddr, err := s.transport.ReadNonQUICPacket(ctx, resp)
+	m, respAddr, err := rdr(ctx, resp)
 	if err != nil {
 		return nil, fmt.Errorf("could not read packet: %w", err)
 	}
--- client/portmap_test.go
+++ client/portmap_test.go
@@ -5,39 +5,13 @@ import (
 	"fmt"
 	"log/slog"
 	"net"
+	"net/netip"
 	"testing"
+	"time"
 
 	"github.com/quic-go/quic-go"
 	"github.com/stretchr/testify/require"
 )
-
-// func TestPCP(t *testing.T) {
-// 	myIP, err := netip.ParseAddr(myIPAddr)
-// 	require.NoError(t, err)
-// 	data, err := pcpMapRequest(myIP)
-// 	require.NoError(t, err)
-// 	fmt.Printf("req: %x\n", data)
-
-// 	addr, err := net.ResolveUDPAddr("udp", ":19190")
-// 	require.NoError(t, err)
-// 	udpConn, err := net.ListenUDP("udp", addr)
-// 	require.NoError(t, err)
-// 	defer udpConn.Close()
-
-// 	transport := &quic.Transport{Conn: udpConn}
-// 	defer transport.Close()
-
-// 	gwIP := net.ParseIP(defaultGateway)
-// 	n, err := transport.WriteTo(data, &net.UDPAddr{IP: gwIP, Port: defaultGatewayPort})
-// 	require.NoError(t, err)
-// 	require.EqualValues(t, len(data), n)
-
-// 	var resp = make([]byte, 1100)
-// 	m, resAddr, err := transport.ReadNonQUICPacket(context.Background(), resp)
-// 	require.NoError(t, err)
-// 	fmt.Printf("res from: %s\n", resAddr)
-// 	fmt.Printf("res: %x\n", resp[0:m])
-// }
 
 func TestPMP(t *testing.T) {
 	addr, err := net.ResolveUDPAddr("udp", ":19290")
@@ -52,12 +26,13 @@ func TestPMP(t *testing.T) {
 	pm, err := NewPortmapper(transport, slog.Default())
 	require.NoError(t, err)
 
-	dresult, err := pm.pmpDiscover(context.Background())
-	require.NoError(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-	fmt.Printf("Discover: epoch=%d, addr=%s\n", dresult.epochSeconds, dresult.externalAddr)
+	go pm.Run(ctx)
 
-	mresult, err := pm.pmpMap(context.Background(), 0, 60)
-	require.NoError(t, err)
-	fmt.Printf("Map: epoch=%d, port=%d, lifetime=%d\n", mresult.epochSeconds, mresult.externalPort, mresult.lifetimeSeconds)
+	pm.externalAddrPort.Listen(ctx, func(t *netip.AddrPort) error {
+		fmt.Println("received: ", t)
+		return nil
+	})
 }
