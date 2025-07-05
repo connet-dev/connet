6246b7afeb8ccad8a0f821fb8f541f0370ec176f
Nikolay Petrov <sungon@gmail.com>
2025-07-05 13:26:38 -0400

    better logging and checks

--- nat/pmp.go
+++ nat/pmp.go
@@ -21,6 +21,7 @@ import (
 
 const pmpBroadcastAddr = "224.0.0.1:5350"
 const pmpCommandPort = 5351
+const pmpLifetime = 10 * 60 // 10 minutes
 
 type PMP struct {
 	transport *quic.Transport
@@ -49,10 +50,10 @@ func NewPMP(transport *quic.Transport, logger *slog.Logger) (*PMP, error) {
 }
 
 func (s *PMP) Run(ctx context.Context) error {
-	var boff netc.SpinBackoff
+	boff := netc.SpinBackoff{MinBackoff: time.Second, MaxBackoff: time.Minute}
 	for {
 		err := s.runGeneration(ctx)
-		slogc.Fine(s.logger, "pmp generation completed", "err", err)
+		s.logger.Debug("pmp generation completed", "err", err)
 
 		switch {
 		case errors.Is(err, context.Canceled):
@@ -105,7 +106,7 @@ func (s *PMP) runGeneration(ctx context.Context) error {
 		if err != nil {
 			slogc.Fine(s.logger, "discover external address failed", "gateway", s.gatewayAddr, "err", err)
 		} else {
-			slogc.Fine(s.logger, "discover external address completed", "gateway", s.gatewayAddr, "external-addr", resp.externalAddr)
+			slogc.Fine(s.logger, "discover external address completed", "gateway", s.gatewayAddr, "epoch", resp.epochSeconds, "external-addr", resp.externalAddr)
 		}
 		return resp, err
 	})
@@ -161,11 +162,11 @@ func (s *PMP) notifyExternalAddrPort(ctx context.Context) error {
 func (s *PMP) runMap(ctx context.Context) error {
 	resp, err := retryCall(ctx, func(ctx context.Context) (*pmpMapResponse, error) {
 		slogc.Fine(s.logger, "mapping create start", "gateway", s.gatewayAddr, "local-port", s.localPort)
-		resp, err := s.pmpMap(ctx, 0, 60) // TODO change lifetime
+		resp, err := s.pmpMap(ctx, 0, pmpLifetime) // TODO change lifetime
 		if err != nil {
 			slogc.Fine(s.logger, "mapping create failed", "gateway", s.gatewayAddr, "err", err)
 		} else {
-			slogc.Fine(s.logger, "mapping create completed", "gateway", s.gatewayAddr, "external-port", resp.externalPort)
+			slogc.Fine(s.logger, "mapping create completed", "gateway", s.gatewayAddr, "epoch", resp.epochSeconds, "external-port", resp.externalPort, "lifetime", resp.lifetimeSeconds)
 		}
 		return resp, err
 	})
@@ -195,11 +196,11 @@ func (s *PMP) runMap(ctx context.Context) error {
 		case <-time.After(nextRenew):
 			renewResp, err := retryCall(ctx, func(ctx context.Context) (*pmpMapResponse, error) {
 				slogc.Fine(s.logger, "mapping renew start", "gateway", s.gatewayAddr, "local-port", s.localPort)
-				resp, err := s.pmpMap(ctx, resp.externalPort, 60)
+				resp, err := s.pmpMap(ctx, resp.externalPort, pmpLifetime)
 				if err != nil {
 					slogc.Fine(s.logger, "mapping renew failed", "gateway", s.gatewayAddr, "err", err)
 				} else {
-					slogc.Fine(s.logger, "mapping renew completed", "gateway", s.gatewayAddr, "external-port", resp.externalPort)
+					slogc.Fine(s.logger, "mapping renew completed", "gateway", s.gatewayAddr, "epoch", resp.epochSeconds, "external-port", resp.externalPort, "lifetime", resp.lifetimeSeconds)
 				}
 				return resp, err
 			})
@@ -244,15 +245,12 @@ func (s *PMP) listenAddressChanges(ctx context.Context, epoch uint32) error {
 	}
 
 	for ctx.Err() == nil {
-		resp, err := s.readResponse(ctx, 12, readResponse)
+		resp, err := s.readResponse(ctx, 12, 0, readResponse)
 		if err != nil {
 			if errors.Is(err, os.ErrDeadlineExceeded) {
 				continue
 			}
 			return fmt.Errorf("could not read packet: %w", err)
-		}
-		if err := checkResponseHeader(resp, 0); err != nil {
-			return fmt.Errorf("discovery check response: %w", err)
 		}
 
 		nextEpoch := binary.BigEndian.Uint32(resp[4:])
@@ -281,13 +279,9 @@ func (s *PMP) pmpDiscover(ctx context.Context) (*pmpDiscoverResponse, error) {
 	if err := s.writeRequest(request); err != nil {
 		return nil, fmt.Errorf("discovery write request: %w", err)
 	}
-
-	resp, err := s.readResponse(ctx, 12, s.transport.ReadNonQUICPacket)
+	resp, err := s.readResponse(ctx, 12, 0, s.transport.ReadNonQUICPacket)
 	if err != nil {
 		return nil, fmt.Errorf("discovery read response: %w", err)
-	}
-	if err := checkResponseHeader(resp, 0); err != nil {
-		return nil, fmt.Errorf("discovey check response: %w", err)
 	}
 
 	epoch := binary.BigEndian.Uint32(resp[4:])
@@ -313,13 +307,9 @@ func (s *PMP) pmpMap(ctx context.Context, desiredExternalPort uint16, mappingLif
 	if err := s.writeRequest(request); err != nil {
 		return nil, fmt.Errorf("map write request: %w", err)
 	}
-
-	resp, err := s.readResponse(ctx, 16, s.transport.ReadNonQUICPacket)
+	resp, err := s.readResponse(ctx, 16, 1, s.transport.ReadNonQUICPacket)
 	if err != nil {
 		return nil, fmt.Errorf("map read response: %w", err)
-	}
-	if err := checkResponseHeader(resp, 1); err != nil {
-		return nil, fmt.Errorf("map check response: %w", err)
 	}
 
 	epoch := binary.BigEndian.Uint32(resp[4:])
@@ -367,7 +357,7 @@ func retryCall[T any](ctx context.Context, fn func(ctx context.Context) (T, erro
 
 type readerFn = func(context.Context, []byte) (int, net.Addr, error)
 
-func (s *PMP) readResponse(ctx context.Context, expectedSize int, rdr readerFn) ([]byte, error) {
+func (s *PMP) readResponse(ctx context.Context, expectedSize int, expectedOpcode byte, rdr readerFn) ([]byte, error) {
 	resp := make([]byte, 16)
 	m, respAddr, err := rdr(ctx, resp)
 	if err != nil {
@@ -390,7 +380,12 @@ func (s *PMP) readResponse(ctx context.Context, expectedSize int, rdr readerFn) 
 		return nil, fmt.Errorf("unexpected response address type: %t", t)
 	}
 
-	return resp[0:m], nil
+	resp = resp[0:m]
+	if err := checkResponseHeader(resp, expectedOpcode); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 var errPMPUnsupportedVersion = errors.New("nat-pmp - unsupported version")
