4454456b7cb189e8c279ba74e0a525c38e2e252f
Nikolay Petrov <sungon@gmail.com>
2026-01-10 23:53:40 -0500

    local addresses were not discovered

--- pkg/nat/local.go
+++ pkg/nat/local.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"log/slog"
 	"net/netip"
+	"slices"
+	"time"
 
 	"github.com/connet-dev/connet/pkg/netc"
+	"github.com/connet-dev/connet/pkg/reliable"
 )
 
 type Local struct {
@@ -19,19 +22,38 @@ func NewLocal(localPort uint16, logger *slog.Logger) *Local {
 
 func (s *Local) Get() []netip.AddrPort {
 	localAddrs, err := netc.LocalAddrs()
-	if err == nil {
-		localAddrPorts := make([]netip.AddrPort, len(localAddrs))
-		for i, addr := range localAddrs {
-			localAddrPorts[i] = netip.AddrPortFrom(addr, s.localPort)
-		}
-		return localAddrPorts
-	} else {
+	if err != nil {
 		s.logger.Warn("cannot get local addrs", "err", err)
+		return nil
 	}
 
-	return nil
+	localAddrPorts := make([]netip.AddrPort, len(localAddrs))
+	for i, addr := range localAddrs {
+		localAddrPorts[i] = netip.AddrPortFrom(addr, s.localPort)
+	}
+	return localAddrPorts
+
 }
 
 func (s *Local) Listen(ctx context.Context, fn func([]netip.AddrPort) error) error {
-	return nil
+	locals := s.Get()
+	if err := fn(locals); err != nil {
+		return err
+	}
+
+	for {
+		if err := reliable.WaitDeline(ctx, 30*time.Second); err != nil {
+			return err
+		}
+
+		newLocals := s.Get()
+		if slices.Equal(locals, newLocals) {
+			continue
+		}
+
+		locals = newLocals
+		if err := fn(locals); err != nil {
+			return err
+		}
+	}
 }
